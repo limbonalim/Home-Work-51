c444ae312614d8b5fef3366b772ff4127e473496
Zhuravlev Petr <zeterfrost@gmail.com>
2023-10-17 18:56:25 +0600

    add getRandomNumbers and getNumber function in App.tsx src/App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,33 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Numbers from "./Numbers/Numbers.tsx";
 import {useState} from "react";
-import {Simulate} from "react-dom/test-utils";
-import change = Simulate.change;
+// import {Simulate} from "react-dom/test-utils";
+// import change = Simulate.change;
 
 function App() {
+    const getNumber = (min: number = 5, max: number = 36) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    const getRandomNumbers = (currentMax: number, arr: number[]) => {
+        if (arr.length === 5) {
+            return arr;
+        }
+        let number = getNumber(4, currentMax);
+        if (number > arr[arr.length - 1] || arr.length === 0) {
+            arr.push(number);
+            currentMax += 5;
+        }
+        getRandomNumbers(currentMax, arr)
+    };
 
     const [numbers, setNumbers] = useState([5, 11, 16, 23, 32]);
 
     const changeNumbers = () => {
-        setNumbers([1, 11, 12, 13, 13]);
+        let current = 11;
+        let arrOfNumbers:number[] = [];
+        getRandomNumbers(current, arrOfNumbers)
+        setNumbers(arrOfNumbers);
     };
 
   return (

334b72ede014432a64f2362041d1c5a8b5c7babc
Zhuravlev Petr <zeterfrost@gmail.com>
2023-10-19 15:17:08 +0600

    Refactor code in src/App.tsx src/Numbers/Numbers/tsx src/mail.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
 import './App.css';
-import Numbers from "./Numbers/Numbers.tsx";
-import {useState} from "react";
+import Numbers from './Numbers/Numbers.tsx';
+import {useState} from 'react';
 
 function App() {
-    const getNumber = (min: number = 5, max: number = 36) => {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    };
+  const getNumber = (min: number = 5, max: number = 36) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
 
-    const getRandomNumbers = (currentMax: number, arr: number[]) => {
-        if (arr.length === 5) {
-            return arr;
-        }
-        let number = getNumber(5, currentMax);
-        if (number > arr[arr.length - 1] || arr.length === 0) {
-            arr.push(number);
-            currentMax += 5;
-        }
-        getRandomNumbers(currentMax, arr);
-    };
+  const getRandomNumbers = (currentMax: number, arr: number[]) => {
+    if (arr.length === 5) {
+      return arr;
+    }
+    let number = getNumber(5, currentMax);
+    if (number > arr[arr.length - 1] || arr.length === 0) {
+      arr.push(number);
+      currentMax += 5;
+    }
+    getRandomNumbers(currentMax, arr);
+  };
 
-    const [numbers, setNumbers] = useState([5, 11, 16, 23, 32]);
+  const [numbers, setNumbers] = useState([5, 11, 16, 23, 32]);
 
-    const changeNumbers = () => {
-        let current = 11;
-        let arrOfNumbers:number[] = [];
-        getRandomNumbers(current, arrOfNumbers);
-        setNumbers(arrOfNumbers);
-    };
+  const changeNumbers = () => {
+    let current = 11;
+    let arrOfNumbers: number[] = [];
+    getRandomNumbers(current, arrOfNumbers);
+    setNumbers(arrOfNumbers);
+  };
 
   return (
     <div>
-        <button onClick={changeNumbers}>New numbers</button>
-        <Numbers numbers={numbers}/>
+      <button onClick={changeNumbers}>New numbers</button>
+      <Numbers numbers={numbers}/>
     </div>
   );
 }
--- src/Numbers/Numbers.tsx
+++ src/Numbers/Numbers.tsx
@@ -1,15 +1,15 @@
 const Numbers = ({numbers}) => {
-    return (
-        <div>
-            <div className="numbers-container">
-                <div className="numbers">{numbers[0]}</div>
-                <div className="numbers">{numbers[1]}</div>
-                <div className="numbers">{numbers[2]}</div>
-                <div className="numbers">{numbers[3]}</div>
-                <div className="numbers">{numbers[4]}</div>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="numbers-container">
+        <div className="numbers">{numbers[0]}</div>
+        <div className="numbers">{numbers[1]}</div>
+        <div className="numbers">{numbers[2]}</div>
+        <div className="numbers">{numbers[3]}</div>
+        <div className="numbers">{numbers[4]}</div>
+      </div>
+    </div>
+  );
 };
 
-export default  Numbers;
+export default Numbers;
